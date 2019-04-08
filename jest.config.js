10c0dad3f4bf8f89657e38b5f5550211ee984ce7
Jakub Wadas <vadistic@gmail.com>
2019-04-08 14:01:44 +0200

    chore: better monorepo test/dev support

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,11 @@
 module.exports = {
   preset: 'ts-jest',
   moduleFileExtensions: ['js', 'jsx', 'ts', 'tsx'],
-  roots: ['<rootDir>/tests/', '<rootDir>/src/'],
+  roots: ['<rootDir>/src/'],
   testMatch: ['**/?(*.)+(spec|test).[jt]s?(x)'],
+  moduleNameMapper: {
+    '^@graphql-clientgen\\/([^/]+)': '<rootDir>/../$1/src'
+  },
   globals: {
     'ts-jest': {
       tsConfig: 'tsconfig.test.json'
